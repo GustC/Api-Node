aa4e4daee09271d1166625a4e9b411d90de7d5ac
GustC <gustavogu44@hotmail.com>
2020-02-01 23:59:22 -0300

    Ajuste

--- src/controllers/indexControllers.js
+++ src/controllers/indexControllers.js
@@ -1,14 +1,18 @@
 const sql = require("./../connection/connection");
 const express = require('express');
 exports.getUsers = (req, res, next) => {
-    sql.query("Select * from User",(err,result,columns)=>{
-        if(err){
-            console.log(err);
-            res.status(400).send("Error");
-        }
-        console.log(result);
-        res.status(200).json(result);   
-    });
+    try{
+        sql.query("Select * from User",(err,result,columns)=>{
+            if(err){
+                console.log(err);
+                res.status(400).send("Error");
+            }
+            console.log(result);
+            res.status(200).json(result);   
+        });
+    } catch (err){
+        res.status(400).send("error")
+    }    
 }
 
 
